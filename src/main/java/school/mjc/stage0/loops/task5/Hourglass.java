d3c2b2e76218476c1840097e2aab12e77ac1fda0
NANO <File changed>
2022-08-10 01:55:29 +0600

    5 submission

--- src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
+++ src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
@@ -2,31 +2,25 @@ package school.mjc.stage0.loops.task5;
 
 public class Hourglass {
     public void printHourglassOfGivenSize(int height) {
-
-            for(int i=1; i<=height; i++)
-            {
-                for(int k=1; k<=i-2; k++)
-                {
-                    System.out.println(" ");
-                }
-                for(int j=1; j<=height-i+1; j++)
-                {
-                    System.out.println("8 ");
-                }
-                System.out.println();
+        for (int i = 1; i <= height; i++) {
+            for (int k = 1; k <= i - 1; k++) {
+                System.out.print(" ");
             }
-            for(int i=height-1; i>=1; i--)
-            {
-                for(int k=1; k<=i-2; k++)
-                {
-                    System.out.println(" ");
-                }
-                for(int j=i; j<=height; j++)
-                {
-                    System.out.println("8 ");
-                }
-                System.out.println();
+            for (int j = 1; j <= height - i + 1; j++) {
+                System.out.print("8");
             }
+            System.out.println();
+        }
+        for (int i = 4; i >= 1; i--) {
+            for (int k = 1; k <= i - 1; k++) {
+                System.out.print(" ");
+            }
+            for (int j = i; j <= height; j++) {
+                System.out.print("8");
+            }
+            System.out.println();
         }
     }
+}
+
 
