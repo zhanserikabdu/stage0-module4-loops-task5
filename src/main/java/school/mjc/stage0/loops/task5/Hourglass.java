54aa828313424dddd397b7e15c941fde85fc36da
NANO <File changed>
2022-08-10 01:40:26 +0600

    5 submission

--- src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
+++ src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
@@ -1,42 +1,32 @@
 package school.mjc.stage0.loops.task5;
 
-import java.util.Scanner;
-
 public class Hourglass {
     public void printHourglassOfGivenSize(int height) {
-        int rows, i, j, k;
-        Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter the number of rows: ");
-        rows = sc.nextInt();
-
-        System.out.print("Output: \n\n");
-        //upper half
-        for (i = 1; i <= height; i++)
-        {
-            for (k = 1; k <= i - 2; k++)
+            for(int i=1; i<=height; i++)
             {
-                System.out.print("" + " ");
+                for(int k=1; k<=i-2; k++)
+                {
+                    System.out.println(" ");
+                }
+                for(int j=1; j<=height-i+1; j++)
+                {
+                    System.out.println("8 ");
+                }
+                System.out.println();
             }
-            for (j = 1; j <= height - i + 1; j++)
+            for(int i=height-1; i>=1; i--)
             {
-                System.out.print("8");
+                for(int k=1; k<=i-2; k++)
+                {
+                    System.out.println(" ");
+                }
+                for(int j=i; j<=height; j++)
+                {
+                    System.out.println("8 ");
+                }
+                System.out.println();
             }
-            System.out.println();
-        }
-
-        //lower half
-        for (i = height - 2; i >= 1; i--)
-        {
-            for (k = 1; k <= i - 2; k++)
-            {
-                System.out.print(" ");
-            }
-            for (j = i; j <= height; j++)
-            {
-                System.out.print("8" + " ");
-            }
-            System.out.println();
         }
     }
-}
+
